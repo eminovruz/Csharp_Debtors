f80d3aa2d414080315333590d2571494d7aaa362
eminn <eminn@DESKTOP-3IALIBH>
2022-09-14 01:51:50 +0400

    Proje dosyası ekle.

---
+++ Csharp_Debtors/Methods.cs
@@ -0,0 +1,10 @@
+﻿static class Methods
+{
+    static public void Show<Debtor>(this IEnumerable<Debtor> debtors)
+    {
+        foreach (var debtor in debtors)
+        {
+            Console.WriteLine(debtor);
+        }
+    }
+}
